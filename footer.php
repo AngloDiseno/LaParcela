689efe9506ea252fe054e283718260508647e7b5
Andre Ribet <aribet@gmail.com>
2015-03-13 23:49:23 -0300

    se divide el index en header y footer

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="row">
+<div>Camino Viejo a Cajón, Parcela n°1 <br>Ex-Fundo El Retiro (Frente a Valle de Asturias)<br><strong>(09) 5377 0183 | Temuco</strong></div>
+</footer>
+
+</div>
+</body>
+</html>	
---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang=es>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title><?php bloginfo('name'); ?></title>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>/bootstrap.min.css">
+	<!--<link rel="stylesheet" href="css/estilos.css"> -->
+	<link rel="stylesheet" href="style.css">
+
+<!-- google analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-59417944-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<!-- fin google analytics -->
+
+</head>
+<body>
+<div class="container">
+	<header class="row">
+		<h1><?php bloginfo('name'); ?></h1>
+		<span>alojamiento familiar</span>
+	</header>
